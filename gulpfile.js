040f63c464370a97680970c4ec6f6b28260483d8
thirty33 <joelsuarez.1101@gmail.com>
2017-08-22 17:45:23 -0500

    agregando logo, acomodando rutas de imagenes, creando tarea assets en gulp

--- gulpfile.js
+++ gulpfile.js
@@ -24,66 +24,67 @@ gulp.task('styles', function(){
 		.pipe(sass())
 		.pipe(rename('app.css'))
 		.pipe(gulp.dest('./dist/css'))
-		.pipe(browserSync.stream())
+		// .pipe(browserSync.stream())
 
 
 })
 
-gulp.task('serv', function () {
-	browserSync.init({
-		server: {
-			baseDir: './dist'
-		}
-	})
-	// server.start()
+
+//enviar elementos desde assets a dist(public)
+gulp.task('assets', function(){
+	gulp
+		.src('assets/*')
+		.pipe(gulp.dest('./dist'))
 })
+
+// gulp.task('serv', function () {
+// 	browserSync.init({
+// 		server: {
+// 			baseDir: './dist'
+// 		}
+// 	})
+// 	// server.start()
+// })
 
 // Tarea para procesar el css
 
-gulp.task('css', function(){
-	var processors = [
-		// autoprefixer({browsers : ['> 5%', 'ie 8']}),
-		// atImport(),
-		// mixins(),
-		cssnested,
-		// lost(),
-		rucksack(),
-		cssnext({browsers : ['> 5%', 'ie 8']}),
-		// mqpacker(),
-		// csswring()
-	]
+// gulp.task('css', function(){
+// 	var processors = [
+// 		// autoprefixer({browsers : ['> 5%', 'ie 8']}),
+// 		// atImport(),
+// 		// mixins(),
+// 		cssnested,
+// 		// lost(),
+// 		rucksack(),
+// 		cssnext({browsers : ['> 5%', 'ie 8']}),
+// 		// mqpacker(),
+// 		// csswring()
+// 	]
 
-		return gulp.src('./src/index.css')
-		.pipe(postcss(processors))
-		.pipe(gulp.dest('./dist/css'))
-		.pipe(browserSync.stream())
+// 		return gulp.src('./src/index.css')
+// 		.pipe(postcss(processors))
+// 		.pipe(gulp.dest('./dist/css'))
+// 		.pipe(browserSync.stream())
 		
-})
+// })
 
 // Tarea para vigilar los cambios 
 
-gulp.task('watch', function(){
-	// gulp.watch('./src/*.css', ['css'])
-	gulp.watch('./src/scss/*.scss', ['styles'])
+// gulp.task('watch', function(){
+// 	// gulp.watch('./src/*.css', ['css'])
+// 	gulp.watch('./src/scss/*.scss', ['styles'])
 
-	gulp.watch('./src/js/*.js').on('change', function(){
-		browserSync.reload()
-	})
-	gulp.watch('./dist/*.html').on('change', function(file){
-		browserSync.reload()
-		// server.notify.apply(server,[file])
-	})
+// 	gulp.watch('./src/js/*.js').on('change', function(){
+// 		browserSync.reload()
+// 	})
+// 	gulp.watch('./dist/*.html').on('change', function(file){
+// 		browserSync.reload()
+// 		// server.notify.apply(server,[file])
+// 	})
 
-	// gulp.watch(['./src/*.css', './dist/*.html'], function (file) {
- //      server.notify.apply(server, [file]);
- //    })
-
-	// gulp.watch('./src/*.css').on('change',function(file){
-
-	// 	server.notify.apply(server,[file])
-	// })
-})
+	
+// })
 
 
 // gulp.task('default', ['watch', 'serv', 'css'])
-gulp.task('default', ['watch', 'serv', 'styles'])
+gulp.task('default', ['styles', 'assets'])
